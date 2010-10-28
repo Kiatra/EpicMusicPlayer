b090cdd9416dcc3aec983a1081e2a9bb141d885d
zanony <zanony@xan.at>
2010-10-28 02:35:34 +0000

    add koKR localization

---
+++ EpicMusicPlayer/localization/koKR.lua
@@ -0,0 +1,5 @@
+﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("EpicMusicPlayer", "koKR")
+if not L then return end
+
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="concat")@
