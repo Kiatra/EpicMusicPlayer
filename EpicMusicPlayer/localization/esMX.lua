04026348593cb9cd3949f717d06595ccce4d007b
zanony <zanony@xan.at>
2010-12-26 01:55:55 +0000

    add esMX locale

---
+++ EpicMusicPlayer/localization/esMX.lua
@@ -0,0 +1,5 @@
+﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("EpicMusicPlayer", "esMX")
+if not L then return end
+
+--@localization(locale="esMX, format="lua_additive_table", handle-subnamespaces="concat")@
